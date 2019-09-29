a8feb545876025c6e63218cbab07a19d9cb996b6
wt160 <wt160@scarletmail.rutgers.edu>
2019-09-29 19:27:55 -0400

    first commit

---
+++ my_youbot_pkg/src/my_youbot_node.cpp
@@ -0,0 +1,25 @@
+#include <ros/ros.h>
+#include <luh_youbot_controller_api/controller_api.h>
+
+using namespace youbot_api;
+
+int main(int argc, char** argv)
+{
+    // initialise ROS
+    ros::init(argc, argv, "move_arm");
+    ros::NodeHandle node_handle;
+
+    // Initialise arm
+    YoubotArm arm;
+    arm.init(node_handle);
+
+    // Move arm to ARM_UP pose
+    arm.moveToPose("ARM_UP");
+    arm.waitForCurrentAction();
+
+    // Move arm back to HOME pose
+    //arm.moveToPose("DUMP_4_ABOVE");
+    //arm.waitForCurrentAction();
+
+    return 0;
+}
