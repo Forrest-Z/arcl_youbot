0dbae69e21b0c88894befd2f47fbb574a0c6c44b
wei tang <wt160@scarletmail.rutgers.edu>
2019-09-30 10:33:16 -0400

    09/30

--- luh_youbot_gazebo/scripts/base_gazebo_interface.py
+++ luh_youbot_gazebo/scripts/base_gazebo_interface.py
@@ -37,20 +37,47 @@
 ## to the 'chatter' topic
 
 import rospy
-from std_msgs.msg import String
+import math
+import std_msgs.msg
+import geometry_msgs.msg
+import nav_msgs.msg
+import youbot_driver_api
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
 
-if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+from tf.transformations import euler_from_quaternion
+class baseGazeboInterface(youbot_driver_api.baseInterface):
+    def __init__(self, robot_id):
+        self.robot_id = robot_id
+
+    def odom_callback(data):
+        self.odom_msg_ = data
+
+    def init(): 
+        self.cmd_vel_pub_ = rospy.Publisher("gazebo/cmd_vel_" + str(self.robot_id), geometry_msgs.msg.Twist, queue_size=10)
+        rospy.Subscriber("odom_" + str(self.robot_id), nav_msgs.msg.Odometry, odom_callback)
+
+    def readState():
+        self.pose2d = geometry_msgs.msg.Pose2D()
+        self.pose2d.x = self.odom_msg_.pose.pose.position.x
+        self.pose2d.y = self.odom_msg_.pose.pose.position.y
+        (roll, pitch, yaw) = euler_from_quaternion(self.odom_msg_.pose.pose.orientation)
+        if yaw > math.pi:
+            yaw -= 2 * math.pi
+        elif yaw < - math.pi:
+            yaw += 2 * math.pi
+        self.pose2d.theta = yaw
+        
+    def writeCommand():
+        self.cmd_vel_pub_.publish(velocity_command_)
+
+
+
+
+
+# if __name__ == '__main__':
+#     rospy.init_node("base_gazebo_interface", anonymous=True)
+#     try:
+#         in = baseGazeboInterface(0)
+#         in.writeCommand()
+#     except rospy.ROSInterruptException:
+#         pass
