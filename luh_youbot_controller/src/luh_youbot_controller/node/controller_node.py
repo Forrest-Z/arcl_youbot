e64970e5385203c70ef3d82c363840826e66e5d1
wt160 <wt160@scarletmail.rutgers.edu>
2019-10-03 16:05:29 -0400

    add function: new cmd, move, no cmd, no move

--- luh_youbot_controller/src/luh_youbot_controller/node/controller_node.py
+++ luh_youbot_controller/src/luh_youbot_controller/node/controller_node.py
@@ -8,10 +8,13 @@ import luh_youbot_controller.module_base_controller.base_controller as base_cont
 class controller_node():
     
     def __init__(self):
+        self.base_frequency = rospy.get_param('luh_youbot_controller/base_controller_frequency', 50.0)
+        self.arm_frequency = rospy.get_param('luh_youbot_controller/arm_controller_frequency', 200.0)
         self.is_using_robot = rospy.get_param('arcl_youbot_controller/use_youbot', False)
         self.is_using_gazebo = rospy.get_param('arcl_youbot_controller/use_gazebo', True)
         self.robot_id = rospy.get_param('arcl_youbot_controller/robot_id', 0)
-        rospy.Timer(rospy.Duration(1.0/10.0), self.base_timer_callback)
+        rospy.Timer(rospy.Duration(1.0 / self.base_frequency), self.base_timer_callback)
+        rospy.Timer(rospy.Duration(1.0 / self.arm_frequency), self.arm_timer_callback)
         if self.is_using_robot == True:
             self.youbot_ = youbot_interface.YoubotInterface(self.robot_id)
         elif self.is_using_gazebo == True:
@@ -22,7 +25,7 @@ class controller_node():
 
 
     def base_timer_callback(self, event=None):
-        print "enter t_callback in" + rospy.get_name() 
+       # print "enter t_callback in" + rospy.get_name() 
         self.youbot_.base_interface.read_state()
 
         self.base_controller_module.update()
@@ -31,7 +34,15 @@ class controller_node():
 
         self.youbot_.base_interface.publish_message()
 
+    def arm_timer_callback(self, event=None):
+        pass
+        # self.youbot_.arm_interface.read_state()
 
+        # self.arm_controller_module.update()
+
+        # self.youbot_.arm_interface.write_command()
+
+        # self.youbot_.arm_interface.publish_message()
 if __name__ == "__main__":
     rospy.init_node('controller_node')
     node = controller_node()
--- luh_youbot_gazebo/src/luh_youbot_gazebo/base_gazebo_interface.py
+++ luh_youbot_gazebo/src/luh_youbot_gazebo/base_gazebo_interface.py
@@ -49,13 +49,20 @@ class baseGazeboInterface(baseInterface):
         self.robot_id = robot_id
         self.init()
         self.odom_msg_ = nav_msgs.msg.Odometry()
-
-    def odom_callback(data):
+        self.velocity_command_ = geometry_msgs.msg.Twist()
+        self.velocity_command_.linear.x = 0 
+        self.velocity_command_.linear.y = 0       
+        self.velocity_command_.linear.z = 0       
+        self.velocity_command_.angular.x = 0
+        self.velocity_command_.angular.y = 0
+        self.velocity_command_.angular.z = 0
+    
+    def odom_callback(self, data):
         self.odom_msg_ = data
 
     def init(self): 
         self.cmd_vel_pub_ = rospy.Publisher("gazebo/cmd_vel", geometry_msgs.msg.Twist, queue_size=10)
-        #rospy.Subscriber("odom_" + str(self.robot_id), nav_msgs.msg.Odometry, odom_callback)
+        rospy.Subscriber("odom", nav_msgs.msg.Odometry, self.odom_callback)
 
     def read_state(self):
         self.pose2d = geometry_msgs.msg.Pose2D()
@@ -71,9 +78,11 @@ class baseGazeboInterface(baseInterface):
         elif yaw < - math.pi:
             yaw += 2 * math.pi
         self.pose2d.theta = yaw
+        print "pos:" + str(self.pose2d.x) + ", " + str(self.pose2d.y) + ", " + str(self.pose2d.theta)
         
     def write_command(self):
         self.cmd_vel_pub_.publish(self.velocity_command_)
+        #print "current vel cmd:" + str(self.velocity_command_.linear.x) + ", " + str(self.velocity_command_.linear.y) + ", " + str(self.velocity_command_.angular.z)
     
     def set_velocity(self, vel):
         self.velocity_command_ = vel
