91dd8adc5f7f98ba6df808e41ac621f2f574c66d
baichuan55555 <baichuan55555@gmail.com>
2019-10-18 09:16:56 -0400

    base planner - visibility graph - previous bug was resolved

--- arcl_youbot_planner/src/arcl_youbot_planner/base_planner/visgraph/README.md
+++ arcl_youbot_planner/src/arcl_youbot_planner/base_planner/visgraph/README.md
@@ -1,1 +1,3 @@
-taken from https://github.com/TaipanRex/pyvisgraph and made some changes for youbot [2019.10.16]
+taken from https://github.com/TaipanRex/pyvisgraph [2019.10.16] and made some changes for youbot
+
+Used `pip install shapely` for Minkowski sum (dilation)
--- arcl_youbot_planner/src/arcl_youbot_planner/base_planner/visgraph/vg_youbot_path.py
+++ arcl_youbot_planner/src/arcl_youbot_planner/base_planner/visgraph/vg_youbot_path.py
@@ -1,7 +1,9 @@
 import arcl_youbot_planner.base_planner.visgraph as vg
+from arcl_youbot_planner.base_planner.visgraph.visible_vertices import edge_distance
 import commands
 from shapely.geometry import Polygon
 from shapely.ops import unary_union
+import math
 
 YOUBOT_RADIUS = 0.3  # in meters
 TEST = True
@@ -34,13 +36,46 @@ def vg_find_path(start_pos, goal_pos, obstacles):
         g.build(polygons, workers=cpu_cores)
     path = g.shortest_path(vg.Point(start_pos[0], start_pos[1]), vg.Point(goal_pos[0], goal_pos[1]))
 
+    print(obstacles)
+    print(polygons)
+
     return path
+
+def add_orientation(path, start_heading, goal_heading):
+    """
+    Assume the difference between start_heading and goal_heading is less than pi
+    """
+
+    # Calculate the distance between two points
+    step_distance = []
+    prev_point = path[0]
+    for point in path[1:]:
+        step_distance.append(edge_distance(prev_point, point))
+        prev_point = point
+    path_distance = sum(step_distance)
+
+    # total rotation
+    delta_rotation = goal_heading - start_heading
+
+    # create list of path with heading
+    current_heading = start_heading
+    path_with_heading = [(path[0].x, path[0].y, current_heading)]
+    for i in range(1, len(path)):
+        current_heading += delta_rotation * step_distance[i-1] / path_distance
+        path_with_heading.append((path[i].x, path[i].y, current_heading))
+
+    return path_with_heading
 
 if __name__ == "__main__":
     start_pos = (0, 0)
     goal_pos = (5, 5)
-    obstacles = [[(1, 1), (2, 1.5), (1, 1.5)],
+    obstacles = [[(1, 1), (2, 1.5), (1, 2)],
                  [(3, 2), (4, 2), (4, 4), (3, 4)],
                  [(4, 3), (4.5, 1), (5, 3)]]
     path = vg_find_path(start_pos, goal_pos, obstacles)
-    print(path)
+
+    start_heading = 0
+    goal_heading = math.pi / 2
+    path_with_heading = add_orientation(path, start_heading, goal_heading)
+
+    print(path_with_heading)
