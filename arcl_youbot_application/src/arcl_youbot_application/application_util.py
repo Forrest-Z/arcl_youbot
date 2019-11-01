fixed a bug for velocity controller, the normalization made velocity over max_velocity

arcl_youbot_application/src/arcl_youbot_application/application_util.py
@@ -14,7 +14,7 @@ from gazebo_msgs.srv import SpawnModel
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Pose2D
 from std_msgs.msg import String
-from std_msgs.msg import UInt8
+from std_msgs.msg import UInt8, Float64
 import arcl_youbot_planner.arm_planner.astar 
 from tf.transformations import euler_from_quaternion, quaternion_from_euler, quaternion_about_axis, quaternion_from_matrix
 import scipy.spatial
@@ -27,6 +27,7 @@ from arcl_youbot_application.msg import SceneObjectMsg
 import arcl_youbot_planner.base_planner.visgraph as vg
 import arcl_youbot_planner.arm_planner.prmstar as prmstar
 import arcl_youbot_application.common_util as common_util
+from youbot_forklift_ros_interface.msg import GoToPositionAction, GoToPositionGoal
 import os.path
 
 GAZEBO_COLORS = [ 
@@ -353,3 +354,20 @@ class YoubotEnvironment():
         [final_path, final_cost] = prmstar_planner.direct_path(tuple(start), tuple(pre_pick_joint_value))
         arm_util.execute_path(youbot_name, final_path)
         print("moved back to the pre_pick pose")
+
+    def forklift_done_cb(self, goal_state, result):
+        print("Fork Lift GoToPosition returned")
+        print(result)
+
+    def set_forklift_position(self, youbot_name, position, position_reached=None, position_error=None):
+        client = actionlib.SimpleActionClient(youbot_name + "ForkLift", GoToPositionAction)
+        client.wait_for_server()
+        goal = GoToPositionGoal()
+        goal.goal_position_in_meter = position
+        if position_reached:
+            goal.position_reached = position_reached
+        if position_error:
+            goal.position_error = position_error
+        client.send_goal(goal, done_cb=self.forklift_done_cb)
+        client.wait_for_result(rospy.Duration.from_sec(10.0))
+        return client.get_result()

arcl_youbot_planner/src/arcl_youbot_planner/base_planner/velocity_controller.py
@@ -1,12 +1,13 @@
 import rospy
 from pid import PID
 from geometry_msgs.msg import Twist
-from math import cos, sin, pi
+from math import cos, sin, pi, sqrt
 from base_util import get_youbot_base_pose2d
 from tf.transformations import euler_from_quaternion
 from gazebo_msgs.msg import ModelStates
 
 ALPHA = 0.2
+VEL_MAX = 0.42426
 
 class VelocityController(object):
     """ Send velocity command to follow a path based on current youbot position. """
@@ -114,21 +115,26 @@ class VelocityController(object):
 
             # normalization, because we limited velocity
             if velocity[0] != 0 and velocity[1] != 0 and diff_pos[0] != 0 and diff_pos[1] != 0:
-                if abs(abs(velocity[0] / velocity[1]) - abs(diff_pos[0] / diff_pos[1])) > 0.1:
-                    if abs(velocity[0]) > abs(velocity[1]):
-                        velocity[0] = diff_pos[0] / diff_pos[1] * velocity[1]
-                    else:
+                if abs(velocity[0] / velocity[1] - diff_pos[0] / diff_pos[1]) > 0.01:
+                    if abs(velocity[0]) == self.x_pid.output_limits[1] and abs(velocity[1]) == self.y_pid.output_limits[1]:
+                        diff_pos_norm = sqrt(diff_pos[0]**2 + diff_pos[1]**2)
+                        velocity[0] = diff_pos[0] / diff_pos_norm * VEL_MAX
+                        velocity[1] = diff_pos[1] / diff_pos_norm * VEL_MAX
+                    elif abs(velocity[0]) == self.x_pid.output_limits[1]:
                         velocity[1] = velocity[0] / diff_pos[0] * diff_pos[1]
-
+                    elif abs(velocity[1]) == self.y_pid.output_limits[1]:
+                        velocity[0] = diff_pos[0] / diff_pos[1] * velocity[1]
 
             self.velocity.linear.x = velocity[0]
             self.velocity.linear.y = velocity[1]
             self.velocity.angular.z = velocity[2]
 
-            print("-----" + str(self.step) + "-----")
-            print(self.current_pos)
-            print(current_step_pos)
-            print(velocity)
+            
+            if velocity[0] > 0.3 or velocity[1] > 0.3:
+                print("-----" + str(self.step) + "-----")
+                print(self.current_pos)
+                print(current_step_pos)
+                print(velocity)
         else:
             self.velocity.linear.x = 0
             self.velocity.linear.y = 0